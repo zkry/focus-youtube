f35246a5168db53473f353e250381b1f6dee8d58
Zachary Romero <zacromero3@gmail.com>
2018-08-19 23:33:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Focus - YouTube
+
+The purpose of this extension is to help people develop a more healthy relationship with YouTube. Often times people find themselves spending too much time than expected due to some menacing social engineering. One of the things that keeps users spending the most time on YouTube is the 'recommendation' feature where, based on historical data, YouTube recommends videos that the user is most likely to watch. Eliminating this feature helps users to spend time on exactly what they meant to do on YouTube.
+
+Usage is very simple. Install the extension from here(?) and enjoy your improved ability to focus!
---
+++ src/content-script.js
@@ -0,0 +1,59 @@
+var PAGETYPE_WATCH = "watch";
+var PAGETYPE_HOME = "home";
+
+window.onload = function() {
+  if (pageType() == PAGETYPE_HOME) {
+    // Remove the pages of recommendations
+    var interval =  window.setInterval(function() {
+      var content = document.getElementById("primary")
+      if (content) {
+        content.innerHTML = mainPageHTML()
+      }
+
+      var guides = document.querySelector("ytd-guide-renderer")
+      if (guides) {
+        guides.innerHTML = ""
+      }
+    }, 1000);
+  }
+
+  if (pageType() == PAGETYPE_WATCH) {
+    var interval = window.setInterval(function() {
+      // Remove the side bar
+      var sideItem = document.querySelector("ytd-watch-next-secondary-results-renderer")
+      if (sideItem) {
+        sideItem.innerHTML = ""
+      }
+
+      // Remove the comment section
+      var comments = document.querySelector("ytd-comments")
+      if (comments) {
+        comments.innerHTML = ""
+      }
+    }, 1000)
+  }
+}
+
+function pageType() {
+  var href = window.location.href;
+  if (href.includes("/watch?")) {
+    return PAGETYPE_WATCH
+  }
+  if (window.location.pathname.split("/").length <= 2) {
+    return PAGETYPE_HOME
+  }
+}
+
+function mainPageHTML() {
+  return `
+  <div>
+    <h3 style="text-align: center;">You probably didn't come here to waste the next hour of your life.</h3>
+    <h4 style="text-align: center;">Please try to think of what you came here for, write that in the search box above, find your vidoe, and learn something new.</h4>
+    <div>Probably the only things that should be clicked on that appear on this page:</div>
+    <ul>
+      <li><a href="/feed/history">History</a></li>
+      <li><a href="/account">Account</a></li>
+    </ul>
+  </div>
+  `
+}
